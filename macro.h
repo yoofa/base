237052a91cc247f0bafd00440e79878b875e040b
youfa.song <vsyfar@gmail.com>
2023-09-09 20:28:15 +0800

    feat: 🎸 add arraysize macro

---
+++ macro.h
@@ -0,0 +1,26 @@
+/*
+ * macro.h
+ * Copyright (C) 2023 youfa <vsyfar@gmail.com>
+ *
+ * Distributed under terms of the GPLv2 license.
+ */
+
+#ifndef MACRO_H
+#define MACRO_H
+
+#include <stddef.h>
+
+// The arraysize(arr) macro returns the # of elements in an array arr.
+// The expression is a compile-time constant, and therefore can be
+// used in defining new arrays, for example.  If you use arraysize on
+// a pointer by mistake, you will get a compile-time error.
+
+// This template function declaration is used in defining arraysize.
+// Note that the function doesn't need an implementation, as we only
+// use its type.
+template <typename T, size_t N>
+char (&ArraySizeHelper(T (&array)[N]))[N];
+
+#define arraysize(array) (sizeof(ArraySizeHelper(array)))
+
+#endif /* !MACRO_H */
