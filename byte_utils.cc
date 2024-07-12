7b4621d3c310ea8cf2094fde03d104384146a3df
youfa <vsyfar@gmail.com>
2024-07-12 13:29:59 +0800

    fix: 🐛 byte_utils type error

--- byte_utils.cc
+++ byte_utils.cc
@@ -18,7 +18,7 @@ uint32_t U32_AT(const uint8_t* ptr) {
 }
 
 uint64_t U64_AT(const uint8_t* ptr) {
-  return ((uint64_t)U32_AT(ptr)) << 32 | U32_AT(ptr + 4);
+  return static_cast<uint64_t>(U32_AT(ptr)) << 32 | U32_AT(ptr + 4);
 }
 
 uint16_t U16LE_AT(const uint8_t* ptr) {
@@ -30,23 +30,23 @@ uint32_t U32LE_AT(const uint8_t* ptr) {
 }
 
 uint64_t U64LE_AT(const uint8_t* ptr) {
-  return ((uint64_t)U32LE_AT(ptr + 4)) << 32 | U32LE_AT(ptr);
+  return static_cast<uint64_t>(U32LE_AT(ptr + 4)) << 32 | U32LE_AT(ptr);
 }
 
 // XXX warning: these won't work on big-endian host.
 uint64_t ntoh64(uint64_t x) {
-  return ((uint64_t)ntohl(x & 0xffffffff) << 32) | ntohl(x >> 32);
+  return (static_cast<uint64_t>(ntohl(x & 0xffffffff)) << 32) | ntohl(x >> 32);
 }
 
 uint64_t hton64(uint64_t x) {
-  return ((uint64_t)htonl(x & 0xffffffff) << 32) | htonl(x >> 32);
+  return (static_cast<uint64_t>(htonl(x & 0xffffffff)) << 32) | htonl(x >> 32);
 }
 
 void MakeFourCCString(uint32_t x, char* s) {
-  s[0] = x >> 24;
-  s[1] = (x >> 16) & 0xff;
-  s[2] = (x >> 8) & 0xff;
-  s[3] = x & 0xff;
+  s[0] = static_cast<char>(x >> 24);
+  s[1] = static_cast<char>((x >> 16) & 0xff);
+  s[2] = static_cast<char>((x >> 8) & 0xff);
+  s[3] = static_cast<char>(x & 0xff);
   s[4] = '\0';
 }
 } /* namespace ave */
--- byte_utils.h
+++ byte_utils.h
@@ -10,7 +10,7 @@
 
 #include <arpa/inet.h>
 
-#include "base/types.h"
+#include "base/errors.h"
 
 namespace ave {
 
